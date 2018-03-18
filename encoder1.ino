69aede2dfdc4bf06334512310706e5dc71246b24
PrajayVora <voraprajay@gmail.com>
2018-03-18 13:11:27 +0530

    Add files via upload

---
+++ encoder1.ino
@@ -0,0 +1,39 @@
+
+int val; 
+int encoder0PinA = 4;
+int encoder0PinB = 5;
+int encoder0Pos = 0;
+int encoder0PinALast = LOW;
+int n = LOW;
+float setpoint = 0;
+float angle = 0;
+float error = 0;
+unsigned long lastTime;
+float errSum, lastErr;
+float kp;
+float ki;
+float kd;
+ 
+void setup() { 
+    pinMode (encoder0PinA,INPUT);
+    pinMode (encoder0PinB,INPUT);
+    Serial.begin (9600);
+} 
+
+void loop() { 
+    n = digitalRead(encoder0PinA);
+    if ((encoder0PinALast == LOW) && (n == HIGH)) {
+      if (digitalRead(encoder0PinB) == LOW) {
+        encoder0Pos--;
+      } else {
+        encoder0Pos++;
+      }
+      Serial.print (encoder0Pos);
+      Serial.print ("/");
+    } 
+    encoder0PinALast = n;
+   
+    angle = encoder0Pos; 
+    error = angle - setpoint;
+    
+} 
---
+++ invertedpendumumpot.ino
@@ -0,0 +1,75 @@
+/*working variables*/
+int motor[]={6,9};
+unsigned long lastTime;
+double Input, Output;
+double Setpoint=678;
+double errSum, lastErr;
+double kp=50;
+double ki=30;
+double kd=10;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(9600);
+  int i;
+  for(i=0;i<2;i++)
+      pinMode(motor[i],OUTPUT);
+}
+
+                                        // the loop routine runs over and over again forever:
+void loop() {                           // read the input on analog pin 0:
+  int pot = analogRead(A0);     // print out the value you read:
+  Serial.println(pot);
+  delay(0);                             // delay in between reads for stability
+
+  if (pot>Setpoint){
+   Input=pot;
+   Compute();
+   moveForward();
+  }
+  else if(pot<Setpoint){
+   Input=pot;
+   Compute();
+   moveBackward();
+  }
+  
+}
+
+
+void Compute()
+{
+   /*How long since we last calculated*/
+   unsigned long now = millis();
+   double timeChange = (double)(now - lastTime);
+  
+   /*Compute all the working error variables*/
+   double error = Setpoint - Input;
+   Serial.println(error);
+   errSum += (error * timeChange);
+   double dErr = (error - lastErr) / timeChange;
+  
+   /*Compute PID Output*/
+   Output = kp * error + ki * errSum + kd * dErr;
+  Serial.println(Output);
+   /*Remember some variables for next time*/
+   lastErr = error;
+   lastTime = now;
+  
+}
+
+
+void moveForward()
+{
+  digitalWrite(motor[0], LOW);
+  analogWrite(motor[1],Output/50000);
+  
+}
+
+void moveBackward()
+{
+  analogWrite(motor[0],Output/50000);
+  digitalWrite(motor[1],LOW);
+}
+
+
+
